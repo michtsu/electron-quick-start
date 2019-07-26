93aa0ea71e4d5817bc88d84a88cf9ad026c10d75
Michael Tsu <michtsu@microsoft.com>
2019-07-26 10:57:00 -0700

    Changes to test debugging multiple shared workers

--- main.js
+++ main.js
@@ -1,19 +1,34 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const {app, Menu, MenuItem, BrowserWindow} = require('electron')
+const path = require('path')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
+let sharedWorkers
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    // Don't show the window until it ready, this prevents any white flickering
+    show: false,    
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
 
   // and load the index.html of the app.
   mainWindow.loadFile('index.html')
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
+
+  // Show window when page is ready
+  mainWindow.once('ready-to-show', () => {
+    mainWindow.show()
+  })
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
@@ -22,6 +37,8 @@ function createWindow () {
     // when you should delete the corresponding element.
     mainWindow = null
   })
+
+  buildMenuOptions();
 }
 
 // This method will be called when Electron has finished
@@ -33,18 +50,72 @@ app.on('ready', createWindow)
 app.on('window-all-closed', function () {
   // On macOS it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+  if (process.platform !== 'darwin') app.quit()
 })
 
 app.on('activate', function () {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow()
-  }
+  if (mainWindow === null) createWindow()
 })
+
+function buildSharedWorkerMenuOptions(focusedWindow) {
+    let sharedWorkerMenu = [];
+    if (focusedWindow) {
+      const sharedWorkers = focusedWindow.webContents.getAllSharedWorkers();
+      sharedWorkers.forEach((sharedWorker) => {
+        console.log('Appending menu item for shared worker ' + sharedWorker.url);
+        sharedWorkerMenu.push({
+          label: 'Debug shared worker ' + sharedWorker.id,
+          click (item, focusedWindow) {
+            focusedWindow.webContents.inspectSharedWorkerById(sharedWorker.id)
+          }      
+        });
+      });
+      const template = [
+        {
+          label: 'Debug shared workers',
+          submenu: sharedWorkerMenu
+        }
+      ];
+      const menu = Menu.buildFromTemplate(template)
+      Menu.setApplicationMenu(menu)      
+  }
+}
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+function buildMenuOptions() {
+  const template = [
+    {
+      label: 'Teams',
+      submenu: [
+        {
+          label: 'Reload',
+          accelerator: 'CmdOrCtrl+R',
+          click (item, focusedWindow) {
+            if (focusedWindow) focusedWindow.reload()
+          }
+        },
+        {
+          label: 'Toggle Developer Tools',
+          accelerator: process.platform === 'darwin' ? 'Alt+Command+I' : 'Ctrl+Shift+I',
+          click (item, focusedWindow) {
+            if (focusedWindow) focusedWindow.webContents.toggleDevTools()
+          }
+        },
+        {
+          label: 'Debug shared workers',
+          accelerator: process.platform === 'darwin' ? 'Alt+Command+I' : 'Ctrl+Shift+S',
+          click (item, focusedWindow) {
+            if (focusedWindow) {
+              buildSharedWorkerMenuOptions(focusedWindow);
+            }
+          }
+        },
+      ]
+    },
+  ]
+  const menu = Menu.buildFromTemplate(template)
+  Menu.setApplicationMenu(menu)
+}
--- renderer.js
+++ renderer.js
@@ -1,3 +1,8 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+var worker1 = new SharedWorker('shared-worker1.js');
+var worker2 = new SharedWorker('shared-worker2.js');
+
+worker1.port.start();
+worker2.port.start();
